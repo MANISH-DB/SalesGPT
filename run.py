d6ca5fd6c2fa000c9ec301a8629d4f84d5ddf030
kelvintsim <83445753+kelvintsim@users.noreply.github.com>
2023-06-27 13:51:56 +0800

    Initalize integration with streamlit chat

--- run.py
+++ run.py
@@ -1,61 +1,83 @@
 import argparse
+import streamlit as st
+from streamlit_chat import message
 
 import os
 import json
 
 from salesgpt.agents import SalesGPT
-from langchain.chat_models import ChatOpenAI
+from langchain.chat_models import AzureChatOpenAI
 
 
-if __name__ == "__main__":
 
-    # import your OpenAI key (put in your .env file)
-    with open('.env','r') as f:
-        env_file = f.readlines()
-    envs_dict = {key.strip("'") :value.strip("\n") for key, value in [(i.split('=')) for i in env_file]}
-    os.environ['OPENAI_API_KEY'] = envs_dict['OPENAI_API_KEY']
 
-    # Initialize argparse
-    parser = argparse.ArgumentParser(description='Description of your program')
+# import your OpenAI key (put in your .env file)
+with open('.env','r') as f:
+    env_file = f.readlines()
+envs_dict = {key.strip("'") :value.strip("\n") for key, value in [(i.split('=')) for i in env_file]}
+os.environ['OPENAI_API_KEY'] = envs_dict['OPENAI_API_KEY']
+os.environ["OPENAI_API_TYPE"] = envs_dict["OPENAI_API_TYPE"]
+os.environ["OPENAI_API_BASE"] = envs_dict["OPENAI_API_BASE"]
+os.environ["OPENAI_API_VERSION"] = envs_dict["OPENAI_API_VERSION"]
+os.environ["DEPLOYMENT_NAME"] = envs_dict["DEPLOYMENT_NAME"]
 
-    # Add arguments
-    parser.add_argument('--config', type=str, help='Path to agent config file', default='')
-    parser.add_argument('--verbose', type=bool, help='Verbosity', default=False)
-    parser.add_argument('--max_num_turns', type=int, help='Maximum number of turns in the sales conversation', default=10)
+st.set_page_config(
+    page_title="Sleep Haven",
+    page_icon=":robot:"
+)
+st.header("Sleep Haven")
+st.subheader("Achieve better night's sleep")
 
-    # Parse arguments
-    args = parser.parse_args()
+llm = AzureChatOpenAI(deployment_name = os.environ["DEPLOYMENT_NAME"])
 
-    # Access arguments
-    config_path = args.config
-    verbose = args.verbose
-    max_num_turns = args.max_num_turns
+sales_agent = SalesGPT.from_llm(llm, verbose=False,
+                            salesperson_name="Ted Lasso",
+                            salesperson_role="Sales Representative",
+                            company_name="Sleep Haven",
+                            company_business='''Sleep Haven 
+                            is a premium mattress company that provides
+                            customers with the most comfortable and
+                            supportive sleeping experience possible. 
+                            We offer a range of high-quality mattresses,
+                            pillows, and bedding accessories 
+                            that are designed to meet the unique 
+                            needs of our customers.''')
 
-    llm = ChatOpenAI(temperature=0.9, stop = "<END_OF_TURN>")
 
-    if config_path=='':
-        print('No agent config specified, using a standard config')
-        sales_agent = SalesGPT.from_llm(llm, verbose=verbose)
-    else:
-        with open(config_path,'r') as f:
-            config = json.load(f)
-        print(f'Agent config {config}')
-        sales_agent = SalesGPT.from_llm(llm, verbose=verbose, **config)
 
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
+    st.session_state['history'] = []
+    sales_agent.step()
+    st.session_state.generated.append(sales_agent.conversation_history[-1])
+    st.session_state.history.append(sales_agent.conversation_history[-1])
+    message(st.session_state["generated"], key="initial")
+    print("no history")
+
+if not st.session_state.history:
     sales_agent.seed_agent()
-    print('='*10)
-    cnt = 0
-    while cnt !=max_num_turns:
-        cnt+=1
-        if cnt==max_num_turns:
-            print('Maximum number of turns reached - ending the conversation.')
-            break
-        sales_agent.step()
 
-        # end conversation 
-        if '<END_OF_CALL>' in sales_agent.conversation_history[-1]:
-            print('Sales Agent determined it is time to end the conversation.')
-            break
-        human_input = input('Your response: ')
-        sales_agent.human_step(human_input)
-        print('='*10)
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
+
+def get_text():
+    input_text = st.text_input("You: ")
+    return input_text 
+
+human_input = get_text()
+st.session_state.past.append(human_input)
+
+if human_input:
+    sales_agent.human_step(human_input)
+    sales_agent.conversation_history = st.session_state.history
+    st.session_state.history.append("User: " + human_input + " <END_OF_TURN>")
+    sales_agent.step()
+    st.session_state.generated.append(sales_agent.conversation_history[-1])
+    st.session_state.history.append(sales_agent.conversation_history[-1])
+    st.write(st.session_state)
+
+if st.session_state['generated']:
+    for i in range(len(st.session_state['generated'])-1, -1, -1):
+        message(st.session_state["generated"][i], key=str(i))
+        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+
